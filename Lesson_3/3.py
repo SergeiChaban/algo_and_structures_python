3f40df1d9b7a3ba28e780842bf531d814730eaba
Sergei Chaban <s_chaban@mail.ru>
2019-11-23 15:58:21 +0300

    I started doing work and I will continue.

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,40 @@
 #3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+from random import random
+m = []
+n = []
+N = 20
+f = 0
+f2 = 0
+max_num = 0
+min_num = 0
+count_max = 0
+count_min = 0
+x1 = 0
+x2 = 0
+
+for a in range(N):
+    rand_m = int(random() * 100)
+    m.append(rand_m)
+print(m)
+
+for second_num in range(0, N):
+    if m[second_num] > max_num:
+        count_max += 1
+        if count_max > f:
+            max_num = m[second_num]
+            x1 = second_num
+            f = count_max
+    if second_num > 0:
+        if m[second_num] > min_num:
+            count_min += 1
+            if count_min > f2:
+                min_num = m[second_num]
+                x2 = second_num
+                f2 = count_min
+
+print(max_num, min_num)
+# m[x1] = m[x2]
+# print(m)
+m[x2] = m[x1]
+print(m)
--- Lesson_3/4.py
+++ Lesson_3/4.py
@@ -1,1 +1,28 @@
 # 4.	Определить, какое число в массиве встречается чаще всего.
+from random import random
+
+m = []
+n = []
+N = 20
+f = 0
+
+for a in range(N):
+    rand_m = int(random() * 5)
+    m.append(rand_m)
+print(m)
+
+j = 0
+for first_num in range(j+1, N):
+    m_max = 0
+    for second_num in range(j, N-1):
+        if first_num != second_num and m[first_num] == m[second_num]:
+            m_max += 1
+            if m_max > f:
+                n = m[first_num]
+                f = m_max
+
+
+if f > 0:
+    print('Число:', n, '; повторов:', f+1)
+else:
+    print('Нет повторов')
