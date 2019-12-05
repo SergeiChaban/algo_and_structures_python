53c509b34b771f6e219e7a7488f4b2f9827a6191
Sergei Chaban <s_chaban@mail.ru>
2019-12-05 23:20:50 +0300

    Less6 - guppy3 insalling

--- Lesson_6/1.py
+++ Lesson_6/1.py
@@ -10,39 +10,40 @@
 import sys
 import collections
 from memory_profiler import profile
+from functools import reduce
 
-profit = collections.Counter()
-@profile
-def simple_func(*args, **kwargs):
-
-    q = 4
-
-    av_profit = 0
-
-    for n in range(1, quantity + 1):
-        a = 0
-        for m in range(1, q + 1):
-            s_profit = int(input(f'Введите прибыль для {m} квартала {n} компании '))
-            profit[n] += s_profit
-        print(profit)
-        av_profit = av_profit + profit[n]
-    a = av_profit / quantity
-    print(f'Средняя прибьль {a}')
-    for x in range(1, quantity + 1):
-        if profit[x] > a:
-            print(f'Средняя прибыль компании {x} равна {profit[x]}, это выше среднего')
-        else:
-            print(f'Средняя прибыль компании {x} равна {profit[x]}, это ниже среднего')
-    return profit
-
-
-quantity = int(input('Введите кол-во предприятий  '))
-simple_func(quantity)
-
-print(sys.getrefcount(profit))
-print(sys.getrefcount(quantity))
-print(sys.platform)
-
+# profit = collections.Counter()
+# @profile
+# def simple_func(*args, **kwargs):
+#
+#     q = 4
+#
+#     av_profit = 0
+#
+#     for n in range(1, quantity + 1):
+#         a = 0
+#         for m in range(1, q + 1):
+#             s_profit = int(input(f'Введите прибыль для {m} квартала {n} компании '))
+#             profit[n] += s_profit
+#         print(profit)
+#         av_profit = av_profit + profit[n]
+#     a = av_profit / quantity
+#     print(f'Средняя прибьль {a}')
+#     for x in range(1, quantity + 1):
+#         if profit[x] > a:
+#             print(f'Средняя прибыль компании {x} равна {profit[x]}, это выше среднего')
+#         else:
+#             print(f'Средняя прибыль компании {x} равна {profit[x]}, это ниже среднего')
+#     return profit
+#
+#
+# quantity = int(input('Введите кол-во предприятий  '))
+# simple_func(quantity)
+#
+# print(sys.getrefcount(profit))
+# print(sys.getrefcount(quantity))
+# print(sys.platform)
+#
 
 
 # Line #    Mem usage    Increment   Line Contents
@@ -74,3 +75,47 @@ print(sys.platform)
 # 2
 # 295
 # win32
+# Стандартной памяти достаточно, стек в программе не задействован, обращения к heap не происходит. Оптимизицию делать
+# не нужно
+
+# Пример 4.2 решето Эратосфена
+@profile
+def simple_func(*args, **kwargs):
+    n = int(input("n="))
+
+    for i in range(2, n + 1):
+        for j in lst:
+            if i % j == 0:
+                break
+        else:
+            lst.append(i)
+    return lst
+
+
+lst = []
+simple_func(lst)
+
+
+print(sys.getrefcount(lst))
+print(sys.platform)
+
+
+# Line #    Mem usage    Increment   Line Contents
+# ================================================
+#     81     13.3 MiB     13.3 MiB   @profile
+#     82                             def simple_func(*args, **kwargs):
+#     83     13.3 MiB      0.0 MiB       n = int(input("n="))
+#     84
+#     85     13.3 MiB      0.0 MiB       for i in range(2, n + 1):
+#     86     13.3 MiB      0.0 MiB           for j in lst:
+#     87     13.3 MiB      0.0 MiB               if i % j == 0:
+#     88     13.3 MiB      0.0 MiB                   break
+#     89                                     else:
+#     90     13.3 MiB      0.0 MiB               lst.append(i)
+#     91     13.3 MiB      0.0 MiB       return lst
+#
+#
+# 2
+# win32
+# Стандартной памяти достаточно, стек в программе не задействован, обращения к heap не происходит. Оптимизицию делать
+# не нужно
